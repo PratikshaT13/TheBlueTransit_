c352c017e5b0de0c641b588d5b623b632a13510a
PratikshaT13 <pratikshatigadi@gmail.com>
2024-07-31 19:08:32 +0530

    Update LoginService.js

--- mybus/src/services/LoginService.js
+++ mybus/src/services/LoginService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080'; 
+const API_URL = 'https://servernode-lac.vercel.app'; 
 
 const loginS = async (username, password) => {
     try {
